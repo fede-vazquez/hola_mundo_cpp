d1c4651aceb05578431b34b78287ad66534061af
Vazquez <fvazquez@frsn.utn.edu.ar>
2024-04-11 13:59:56 -0300

    Inicio de ejercico de calcular imc e informar al usuario.

---
+++ imc_info/imc_info.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    [] Iniciar variables para el peso, altura, e imc
+    [] Pedir al usuario que ingrese los datos de peso y altura
+    [] Iniciar la variable de imc con el cálculo de peso sobre altura al cuadrado
+    [] Comparar el resultado del IMC con las distintas posibilidades
+    [] Mostrar la información al usuario
+*/
+
+main(){
+
+
+
+    return 0;
+}

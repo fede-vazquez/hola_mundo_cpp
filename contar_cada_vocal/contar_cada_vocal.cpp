df20eb326648e952e7a7b2b217d0c2eeda66de63
Fede Vazquez <108883172+fede-vazquez@users.noreply.github.com>
2024-06-06 15:40:24 -0300

    Agrega ejercicio de contar cada vocal de un texto por separado.

---
+++ contar_cada_vocal/contar_cada_vocal.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char texto[] = "Hola mundo.";
+
+    char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+    int cantVeces[] = {0, 0, 0, 0, 0};
+
+    int contador = 0;
+    while(texto[contador] != NULL){
+        contador++;
+
+        char caracterMinuscula = tolower(texto[contador]);
+
+        switch(caracterMinuscula){
+            case 'a':
+                cantVeces[0]++;
+                break;
+            case 'e':
+                cantVeces[1]++;
+                break;
+            case 'i':
+                cantVeces[2]++;
+                break;
+            case 'o':
+                cantVeces[3]++;
+                break;
+            case 'u':
+                cantVeces[4]++;
+                break;
+        }
+    }
+
+    for(int i = 0; i < 5; i++){
+        string vezOVeces = (cantVeces[i] == 1) ? " vez." : " veces.";
+
+        cout << "La vocal " << vocales[i] << " se repite " << cantVeces[i] << vezOVeces << endl;
+    }
+
+    return 0;
+}

95ede335ae6b4c4206a1bc0b58a9ee66efc0b33e
FedeVazquez <fedenv2000@gmail.com>
2024-06-10 00:39:43 -0300

    Organiza el código del ejercicio de lista de aprobados.

--- lista_aprobados_ejercicio/lista_aprobados.cpp
+++ lista_aprobados_ejercicio/lista_aprobados.cpp
@@ -15,9 +15,7 @@ using namespace std;
     El programa deberá mostrar una lista con los alumnos aprobados con su respectiva nota y una lista con todos los alumnos que rindieron con su respectiva nota.
 */
 
-int main()
-{
-
+int main(){
     // Pedimos al docente la cantidad de alumnos y la guardamos en una variable
     int cantAlumnos = 0;
     cout << "Ingrese la cantidad de alumnos: ";
@@ -29,9 +27,8 @@ int main()
     vector<float> notas;
     int totalAprobados = 0;
 
-    // Iterar ese n�mero, desde el 0 hasta uno antes.
-    for (int i = 0; i < cantAlumnos; i++)
-    {
+    // Iterar ese número, desde el 0 hasta uno antes.
+    for (int i = 0; i < cantAlumnos; i++){
         cout << endl;
         // Pedir al docente el apellido del alumno y agregarlo al vector de apellidos.
         string apellido;
@@ -43,8 +40,7 @@ int main()
         float notaAlumno = 0;
 
         // Iterar 4 veces, pidiendo al docente la nota de cada ejercicio (del 1 al 4). Sumando cada nota al contador.
-        for (int j = 1; j <= 4; j++)
-        {
+        for (int j = 1; j <= 4; j++){
             float notaEjercicio = 0;
             cout << "Ingrese la nota del ejercicio " << j << " del alumno " << i + 1 << ": ";
             cin >> notaEjercicio;
@@ -55,14 +51,11 @@ int main()
     }
 
     // Iteramos nuevamente, para mostrar a los alumnos aprobados.
-    cout << endl
-         << "Alumnos aprobados:" << endl;
+    cout << endl << "Alumnos aprobados:" << endl;
 
-    for (int i = 0; i < cantAlumnos; i++)
-    {
+    for (int i = 0; i < cantAlumnos; i++){
         // Preguntar si el estudiante aprobó o no con dicha nota.
-        if (notas[i] >= 3)
-        {
+        if (notas[i] >= 3){
             // Si aprobó, lo mostramos en la terminal y sumamos uno al contador de aprobados.
             cout << apellidos[i] << ": " << notas[i] << endl;
             totalAprobados++;
@@ -72,10 +65,8 @@ int main()
     cout << "Total de alumnos aprobados: " << totalAprobados << endl;
 
     // Volvemos a iterar, mostrando todos los alumnos con sus notas.
-    cout << endl
-         << "Nota de todos los alumnos:" << endl;
-    for (int i = 0; i < cantAlumnos; i++)
-    {
+    cout << endl << "Nota de todos los alumnos:" << endl;
+    for (int i = 0; i < cantAlumnos; i++){
         cout << apellidos[i] << ": " << notas[i] << endl;
     }
 
